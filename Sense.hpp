833c8b1bef8b5e878bc4843f6ffc5c2213088576
str8-sho0k <wraithsteamlink@outlook.com>
2024-06-05 21:54:14 -0400

    Update Sense.hpp

--- Sense.hpp
+++ Sense.hpp
@@ -11,13 +11,13 @@ struct Sense {
         this->lp = localPlayer;
         this->players = all_players;
     }
-
+    
     void update(int counter) {
         if (!map->playable)
             return;
         if (!cl->FEATURE_SENSE_ON)
             return;
-
+        
         for (std::size_t i = 0; i < players->size(); i++) {
             Player* p = players->at(i);
             if (!p->isValid())
@@ -27,7 +27,7 @@ struct Sense {
 
             double distance = math::calculateDistanceInMeters(
                 lp->localOrigin.x,
-                lp->localOrigin.y,
+                lp->localOrigin.y, 
                 lp->localOrigin.z,
                 p->localOrigin.x,
                 p->localOrigin.y,
@@ -41,30 +41,29 @@ struct Sense {
                 continue;
             }
 
-            if (p->visible) {
+            if (!p->visible && !p->knocked) {
                 p->setGlowEnable(1);
                 p->setGlowThroughWall(1);
-                int healthShield = p->currentHealth + p->currentShields;
-                p->setCustomGlow(healthShield, true, false);
+                p->setCustomGlow(2, false, true); // Set yellow glow for behind wall
             } else {
                 p->setGlowEnable(1);
-                p->setGlowThroughWall(1);
-                p->setCustomGlow(0, false, true); // Yellow for enemies behind walls
+                p->setGlowThroughWall(0);
+                int healthShield = p->currentHealth + p->currentShields;
+                p->setCustomGlow(healthShield, true, false);
             }
         }
     }
-
+    
     void itemGlow(int counter) {
         if (!map->playable)
             return;
         if (!cl->FEATURE_ITEM_GLOW_ON)
             return;
-
         for (int highlightId = 15; highlightId < 65; highlightId++) {
-            const GlowMode newGlowMode = {137, 0, 0, 127};
+            const GlowMode newGlowMode = { 137,0,0,127 };
             const GlowMode oldGlowMode = mem::Read<GlowMode>(lp->highlightSettingsPtr + (HIGHLIGHT_TYPE_SIZE * highlightId) + 0, "Player oldGlowMode");
             if (newGlowMode != oldGlowMode)
                 mem::Write<GlowMode>(lp->highlightSettingsPtr + (HIGHLIGHT_TYPE_SIZE * highlightId) + 0, newGlowMode);
-        }
+        }         
     }
 };
