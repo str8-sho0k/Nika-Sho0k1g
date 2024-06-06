905365126c414c97676b1b1216baf8c7245a027f
str8-sho0k <wraithsteamlink@outlook.com>
2024-06-06 09:26:58 -0400

    Update LocalPlayer.hpp

--- LocalPlayer.hpp
+++ LocalPlayer.hpp
@@ -1,49 +1,46 @@
 #pragma once
 
+#include "Vectors.hpp"
+#include "Offsets.hpp"
+
 struct LocalPlayer {
     uint64_t base;
     int teamNumber;
     int squadNumber;
-    Vector3D localOrigin;
     Vector3D viewAngles;
-    int currentHealth;
-    int currentShields;
+    Vector3D localOrigin;
     int maxHealth;
     int maxShields;
-    int weaponIndex;
     float traversalStartTime;
     float traversalProgress;
     float traversalReleaseTime;
     float worldtime;
     int frameCount;
-    bool onGround; // Add this line
-
-    bool isValid() {
-        return base != 0;
-    }
+    bool onGround; // This should be defined
+    int highlightSettingsPtr;
+    int weaponEntity;
 
     void setYaw(float yaw) {
         viewAngles.y = yaw;
-        mem::Write<Vector3D>(base + OFF_VIEWANGLES, viewAngles);
+        mem::Write<Vector3D>(base + OFF_VIEW_ANGLES, viewAngles);
     }
 
     void readFromMemory() {
         base = mem::Read<uint64_t>(OFF_REGION + OFF_LOCAL_PLAYER, "LocalPlayer base");
-        if (base == 0) return;
-        teamNumber = mem::Read<int>(base + OFF_TEAM_NUMBER, "LocalPlayer teamNumber");
         squadNumber = mem::Read<int>(base + OFF_SQUAD_NUMBER, "LocalPlayer squadNumber");
-        localOrigin = mem::Read<Vector3D>(base + OFF_LOCAL_ORIGIN, "LocalPlayer localOrigin");
-        viewAngles = mem::Read<Vector3D>(base + OFF_VIEWANGLES, "LocalPlayer viewAngles");
-        currentHealth = mem::Read<int>(base + OFF_CURRENT_HEALTH, "LocalPlayer currentHealth");
-        currentShields = mem::Read<int>(base + OFF_CURRENT_SHIELDS, "LocalPlayer currentShields");
+        viewAngles = mem::Read<Vector3D>(base + OFF_VIEW_ANGLES, "LocalPlayer viewAngles");
         maxHealth = mem::Read<int>(base + OFF_MAX_HEALTH, "LocalPlayer maxHealth");
         maxShields = mem::Read<int>(base + OFF_MAX_SHIELDS, "LocalPlayer maxShields");
-        weaponIndex = mem::Read<int>(base + OFF_WEAPON_INDEX, "LocalPlayer weaponIndex");
         traversalStartTime = mem::Read<float>(base + OFF_TRAVERSAL_START_TIME, "LocalPlayer traversalStartTime");
         traversalProgress = mem::Read<float>(base + OFF_TRAVERSAL_PROGRESS, "LocalPlayer traversalProgress");
         traversalReleaseTime = mem::Read<float>(base + OFF_TRAVERSAL_RELEASE_TIME, "LocalPlayer traversalReleaseTime");
         worldtime = mem::Read<float>(base + OFF_WORLD_TIME, "LocalPlayer worldtime");
         frameCount = mem::Read<int>(base + OFF_FRAME_COUNT, "LocalPlayer frameCount");
         onGround = mem::Read<bool>(base + OFF_ON_GROUND, "LocalPlayer onGround"); // Add this line
+        localOrigin = mem::Read<Vector3D>(base + OFF_LOCAL_ORIGIN, "LocalPlayer localOrigin");
+    }
+
+    bool isValid() {
+        return base != 0;
     }
 };
